86921c792907349d196ffc6fca6815b5567855a6
unknown <iltfhuss@gmail.com>
2024-07-29 23:03:40 +0500

    added code

---
+++ investmentEarning.java
@@ -0,0 +1,15 @@
+
+import java.text.*;
+import java.util.Scanner;
+
+
+public class investmentEarning {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your buying price per share:");
+        double buyingPrice = scan.nextDouble();
+        int day = 1;
+        double closingPrice = 0.1;
+        DecimalFormat df = new DecimalFormat("0.00");
+    }
+}
