5a7e0a0afa96375da088292838e78020bef32a49
unknown <iltfhuss@gmail.com>
2024-08-06 19:57:35 +0500

    added code

--- Grade Point Average/gradePointAverageSystem.java
+++ Grade Point Average/gradePointAverageSystem.java
@@ -1,3 +1,18 @@
 public class gradePointAverageSystem {
-    
+    public static void main(String[] args){
+
+        Student andy = new Student("Andy");
+
+        andy.add("Mathematics", 'C');
+        andy.add("English", 'B');
+        andy.add("History", 'C');
+        andy.add("Geography", 'D');
+
+        System.out.println(andy.getName() + " has a GPA of " + andy.getGPA());
+        System.out.println("It is obtained " + " from these subjects:");
+
+        for(String str: andy.getSubjects()) {
+            System.out.println(str);
+        }
+    }
 }
