c1928db09e8ac72b51d7ad94c06bf71ddbc132d1
unknown <iltfhuss@gmail.com>
2024-08-12 23:58:55 +0500

    added code

--- Bank Account with Transfer Feature/bankAccount.java
+++ Bank Account with Transfer Feature/bankAccount.java
@@ -1,5 +1,8 @@
 
-public class bankAccount {
+public class BankAccount {
+
+    public BankAccount(String sandy) {
+    }
 
     private double balance;
     private String name;
@@ -32,5 +35,17 @@ public class bankAccount {
             System.out.println("Withdrawal by" + name + "fails");
         }
     }
-    
+    public void transfer(double amount, BankAccount acc) {
+        if(this.balance < amount) {
+            System.out.println("Trabsfer fails");
+        }
+        else{
+            this.balance -= amount;
+            acc.balance += amount;
+            System.out.println("Account of " + this.name +
+            " becomes $" + this.balance);
+            System.out.println("Account of "
+                + acc.name + " becomes $" + acc.balance) ;
+        }
+    }
 }
--- Bank Account with Transfer Feature/driver.java
+++ Bank Account with Transfer Feature/driver.java
@@ -1,4 +1,18 @@
 
+import java.util.TooManyListenersException;
+
+
 public class driver {
+    public static void main(String a[]) {
+        BankAccount tom = new BankAccount("Tom");
+        tom.deposit(400.0);
+        BankAccount sandy = new BankAccount("Sandy"); 
+        sandy.deposit(200.0);
+        tom.transfer(100.0, sandy);
+        sandy.withdraw(40.0);
+        sandy.withdraw(700.0);
+        System.out.println(sandy.getBalance());
+
+    }
     
 }
