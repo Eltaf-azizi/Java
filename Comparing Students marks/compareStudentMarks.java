32bc8bb40150681f9a7a96c78f070033102330bb
unknown <iltfhuss@gmail.com>
2024-08-12 22:13:47 +0500

    added code

--- Comparing Students marks/compareStudentMarks.java
+++ Comparing Students marks/compareStudentMarks.java
@@ -1,10 +1,32 @@
 
+import javax.swing.event.ListSelectionEvent;
+
+
 
 public class compareStudentMarks {
-    Student helen = new Student("Helen");
-    helen.addMarks("English", 73);
-    helen.addMarks("Math", 98);
-    helen.addMarks("History", 79);
-    Student leslie = new Student("Leslie");
+    public static void main(String[] a){
+        student helen = new student("Helen");
+        helen.addMarks("English", 73);
+        helen.addMarks("Math", 98);
+        helen.addMarks("History", 79);
+        student leslie = new student("Leslie");
+        leslie.addMarks("English", 89);
+        leslie.addMarks("Math", 97);
+        leslie.addMarks("History", 92);
+        if(helen.compareTo(leslie) > 0) {
+
+            System.out.println(helen.getName()+
+            " gets a higher total mark");
+
+        }
+        else if(helen.compareTo(leslie) < 0) {
+            System.out.println(leslie.getName() + 
+            " gets a highrt total mark");
+        }
+        else {
+            System.out.println("Both students get the same total mark");
+        }
+    }
+
 
 }
