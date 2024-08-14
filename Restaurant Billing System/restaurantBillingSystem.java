1e37e4b8104bbca476f7343dd0d80f9f01d5c9b9
unknown <iltfhuss@gmail.com>
2024-08-14 23:59:54 +0500

    added code

--- Restaurant Billing System/restaurantBillingSystem.java
+++ Restaurant Billing System/restaurantBillingSystem.java
@@ -1,3 +1,7 @@
 public class restaurantBillingSystem {
-    
+    public static void main(String[] args) {
+        String[] dish = {"Sandwich", "Coffee", "Frech Fries", "Salad"};
+        double[] cost = {4.3, 4.3, 3.7, 7.1};
+        bill myBill = new bill();
+    } 
 }
