c866c9f69de582c905842a8593b28b9dd7f8cc67
unknown <iltfhuss@gmail.com>
2024-08-15 00:05:43 +0500

    added code

--- Restaurant Billing System/restaurantBillingSystem.java
+++ Restaurant Billing System/restaurantBillingSystem.java
@@ -3,5 +3,11 @@ public class restaurantBillingSystem {
         String[] dish = {"Sandwich", "Coffee", "Frech Fries", "Salad"};
         double[] cost = {4.3, 4.3, 3.7, 7.1};
         bill myBill = new bill();
+        //pass copies of array references
+        myBill.addOrder("Sandwich", 2, dish, cost);
+        myBill.addOrder("French Fries", 1, dish, cost);
+        myBill.addOrder("Salad", 2, dish, cost);
+        myBill.getOrder();
+        System.out.println(myBill.getTotal());
     } 
 }
