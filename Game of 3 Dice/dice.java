0512ff617318688312562df7a372788b0b2ce782
unknown <iltfhuss@gmail.com>
2024-08-17 22:39:30 +0500

    added code

--- Game of 3 Dice/dice.java
+++ Game of 3 Dice/dice.java
@@ -1,3 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class dice {
-    
+    public static void main(String[] args) {
+
+        int numDice = 3, trials = 7;
+        int playerPoints = 0, computerPoints = 0;
+        Random choice = new Random();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Choose your target number: ");
+        int playerTarget = scan.nextInt();
+        Game player = new Game(numDice, trials);
+        playerPoints = player.play("Player", playerTarget);
+        int computerTarget = choice,nextInt(7) + 1;
+        System.out.println("Computer's target is " + computerTarget);
+        Game computer = new Game(numDice, trials);
+        computerPoints = computer.play("Computer", computerTarget);
+        if(playerPoints > computerPoints) {
+            System.out.println("You win");
+        }
+        else if(playerPoints < computerPoints) {
+            System.out.println("Computer wins");
+        }
+        else System.out.println("Draw");
+        scan.close();
+    }
 }
--- Game of 3 Dice/game.java
+++ Game of 3 Dice/game.java
@@ -1,5 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
 
 
 public class game {
+    int numDice, trials;
+    Random rand;
+    Scanner scan; // for "Enter" key
     
+    public void Game(int numDice, int trials) {
+        this.numDice = numDice;
+        this.trials = trials;
+        rand = new Random();
+        scan = new Scanner(System.in);
+    }
+    public int play(String who, int target) {
+        int [] dice = new int [numDice];
+        int points = 0;
+        for(int i = 0; i<trials;i++){
+            for(int j=0;j<numDice;j++){
+                dice[j] = rand.nextInt(7) + 1;
+            }
+            System.out.println("Press Enter: ");
+            scan.nextLine();
+            for(int j : dice) {
+                System.out.println(j + " ");
+            }
+            System.out.println();
+            for(int j=0;j<numDice; j++){
+                if(dice[j] == target) points++;
+            }
+        }
+
+    }
+
 }
