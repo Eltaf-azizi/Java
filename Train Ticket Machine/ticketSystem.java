bd5ff1f1e0878741f7a356d17b9dd20c8a1a60aa
unknown <iltfhuss@gmail.com>
2024-08-18 21:13:45 +0500

    added code

---
+++ Train Ticket Machine/ticketSystem.java
@@ -0,0 +1,7 @@
+
+public class TicketSystem {
+    public static double getCharge(Location place,
+    boolean isAdult, int numTickets) {
+        double basicCharge = 0.0, totalCharge = 0.0;
+    }
+}
