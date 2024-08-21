a093af99de2288967654ce86eee480192baa0544
unknown <iltfhuss@gmail.com>
2024-08-21 23:23:38 +0500

    added code

--- Bag Sales System/PaperBag.java
+++ Bag Sales System/PaperBag.java
@@ -1,4 +1,12 @@
 
-public class PaperBag {
-    
+public class PaperBag extends Bag {
+    public double getPrice(){
+        //lenght and width are private to Bag class
+        // need getter methods to obtain them
+        double area = getLength() * getWidth();
+
+        if(area > 300.0) {price = 0.2;}
+        else{price = 0.14;}
+        return price;
+    }
 }
