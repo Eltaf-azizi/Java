3100c878e6f3fa692b8765bfc27c45f9d0dd84ec
unknown <iltfhuss@gmail.com>
2024-08-21 23:44:54 +0500

    added code

--- Bag Sales System/PlasticBag.java
+++ Bag Sales System/PlasticBag.java
@@ -1,4 +1,19 @@
 
-public class PlasticBag {
-    
+public class PlasticBag extends Bag {
+    //instance vairable specific to PlasticBag
+    private double tax;
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    private double getTax(){
+        return tax;
+    }
+
+    public double getPrice(){
+        double area = getLength() * getWidth();
+        if(area > 344){price = 0.3;}
+        else{price = 0.2;}
+        return price + tax;
+    }
 }
