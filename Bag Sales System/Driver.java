a0e1e4af4fee0360623409794fcd0937c2bb9d44
unknown <iltfhuss@gmail.com>
2024-08-23 15:10:08 +0500

    added code

--- Bag Sales System/Driver.java
+++ Bag Sales System/Driver.java
@@ -1,4 +1,15 @@
 
 public class Driver {
-    
+    public static void main(String[] args){
+        /*PaperBag paperBag = new PaperBag();
+        paperBag.setLength(10.0);// sentimeters
+        paperBag.setWidth(10.0);
+        System.out.println(paperBag.getPrice());*/
+        PlasticBag plasticBag = new PlasticBag();
+        plasticBag.setLength(10.0);
+        plasticBag.setWidth(10.0);
+        plasticBag.setTax(0.3);
+        System.out.println(plasticBag.getTax());
+        System.out.println(plasticBag.getPrice());
+    }
 }
