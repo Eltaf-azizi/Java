c326439f53f198fd2d5518073c1b8f7b7c0a3b1f
unknown <iltfhuss@gmail.com>
2024-08-24 21:49:40 +0500

    added code

--- Driving License System/CommercialDriver.java
+++ Driving License System/CommercialDriver.java
@@ -2,6 +2,10 @@ public class CommercialDriver extends GenericDriver{
     private String hazardousMaterialsType;
     private String carType; //specific commercial car category
 
+    public CommercialDriver(String string, boolean b, String string2, String string3) {
+        //TODO Auto-generated constructor stub
+    }
+
     public void commercialDriver(String name, boolean isProvisional,
     String materialType, String car){
         super(name, isProvisional);
--- Driving License System/DriverProgram.java
+++ Driving License System/DriverProgram.java
@@ -1,6 +1,30 @@
 
+import java.lang.classfile.instruction.LocalVariableType;
+
+
 public class DriverProgram {
     public static void main(String[] args) {
-        
+        GenericDriver  tom = new GenericDriver("Tom", true);
+        tom.deduct(3);
+        tom.setMedicalReport();
+        CommercialDriver lily = new CommercialDriver("Lily", false,
+        "inflammable", "truck");
+
+        lily.deduct(4);
+        lily.setMedicalReport();
+
+        GenericDriver [] driverArray = new GenericDriver[2];
+        driverArray[0] = tom;
+        driverArray[1] = lily;
+
+        for(int i = 0; i<driverArray.length; i++) {
+            System.out.println(driverArray[i].getName() + "'s license is provisional" +
+             driverArray[i].getIsProvisional());
+             System.out.println(driverArray[i].getName() + " has " + driverArray[i].getPoints());
+             System.out.println(driverArray[i].getMedicalReport());
+        }
+
+        // run a method specific to subclass only
+        System.out.println(Lily.getMaterialsType());
     }
 }
