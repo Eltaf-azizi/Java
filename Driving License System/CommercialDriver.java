44250291e9f72dffa5009db26b98f185216e1c7a
unknown <iltfhuss@gmail.com>
2024-08-24 19:45:03 +0500

    added code

---
+++ Driving License System/CommercialDriver.java
@@ -0,0 +1,9 @@
+public class CommercialDriver extends GenericDriver{
+    private String hazardousMaterialsType;
+    private String carType; //specific commercial car category
+
+    public commercialDriver(String name, boolean isProvisional, String materialType, 
+    String car){
+        
+    }
+}
--- Driving License System/GenericDriver.java
+++ Driving License System/GenericDriver.java
@@ -1,3 +1,42 @@
 public class GenericDriver {
-    
-}
+    private String name;
+    private int points;
+    private boolean isProvisional;
+    // both superclass and subclass can access it
+    protected String medicalReport;
+
+    public GenericDriver(String name, boolean isProvisional) {
+        this.name = name;
+        points = 14;
+        this.isProvisional= isProvisional;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public boolean getIsProvisional() {
+        return isProvisional;
+    }
+
+    public String getMedicalReport(){
+        return medicalReport;
+    }
+
+    public void setMedicalReport() {
+        medicalReport = "Approved to drive private car";
+    }
+
+    public void deduct(int p) {
+        // check if negative points can result
+        if(points >= p){points -= p;}
+        else{points = 0;}
+        if(points == 0) {
+            System.out.println("License Suspended");
+        }
+    }
+} 
