63dda056f4b007cf9f7a5ce117efa945462b6c25
unknown <iltfhuss@gmail.com>
2024-08-29 23:14:18 +0500

    added code

--- Simulate a solar toy car/Driver.java
+++ Simulate a solar toy car/Driver.java
@@ -1,4 +1,17 @@
 
 public class Driver {
-    
+    public static void main(String[] args) {
+        SolarToyCar myCar = new SolarToyCar();
+        System.out.println(myCar.getBattery());
+        myCar.turn(Direction.RIGHT, 44);
+        System.out.println(myCar.getBattery());
+        myCar.forward(30.0);
+        System.out.println(myCar.getBattery());
+        myCar.turnWithSolarEnergy(20.0);
+        System.out.println(myCar.getBattery());
+        myCar.moveWithSolarEnergy(100.0);
+        System.out.println(myCar.getBattery());
+        myCar.chargeBattery();
+        System.out.println(myCar.getBattery());
+    }
 }
--- Simulate a solar toy car/SolarToyCar.java
+++ Simulate a solar toy car/SolarToyCar.java
@@ -7,5 +7,11 @@ public class SolarToyCar extends ToyCar implements SolarPanel{
         this.batteryLevel = fullBattery;
     }
 
-    public void moveWithSolarEnergy
+    public void moveWithSolarEnergy(double distance) {
+        System.out.println("Move for " + distance + " meters without using battery power");
+    }
+
+    public void turnWithSolarEnergy(double angle) {
+        System.out.println("Turn for " + angle + " degrees without using battery power");
+    }
 }
