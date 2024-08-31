65e6d07bf4b59f6d52e6152eb3c0876a34b5a7c9
unknown <iltfhuss@gmail.com>
2024-08-31 22:12:27 +0500

    added code

--- Multiple Choice Test/Question.java
+++ Multiple Choice Test/Question.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Question {
     private String question;
@@ -9,5 +10,22 @@ public class Question {
     public Question(String question, String[] choices, String aswer) {
         this.question = question;
         this.choices = new ArrayList<String>();
+        for(int i=0;i<choices.length;i++) {
+            this.choices.add(choices[i]);
+        }
+        // make sure each shows questions in defferent order
+        Collections.shuffle(this.choices);
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public ArrayList<String> getChoices() {
+        return choices;
+    }
+
+    public String getAnswer() {
+        return answer;
     }
 }
