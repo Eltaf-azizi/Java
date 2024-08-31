c6da00d1f1b082e746889acc89837f8c39658ac6
unknown <iltfhuss@gmail.com>
2024-08-31 22:48:04 +0500

    added code

--- Multiple Choice Test/Game.java
+++ Multiple Choice Test/Game.java
@@ -1,4 +1,11 @@
 
+import java.util.ArrayList;
+
+
 public class Game {
-    
+    private ArrayList<Question> questionSet;
+
+    public Game() {
+        questionSet = new ArrayList<Question>();
+    }
 }
