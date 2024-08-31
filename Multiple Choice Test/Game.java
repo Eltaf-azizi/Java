e880491220e810dbb8923a6ff1dc3f9a02b8a9f3
unknown <iltfhuss@gmail.com>
2024-08-31 23:01:36 +0500

    added code

--- Multiple Choice Test/Game.java
+++ Multiple Choice Test/Game.java
@@ -7,5 +7,8 @@ public class Game {
 
     public Game() {
         questionSet = new ArrayList<Question>();
+        String q = "Where does the sun rize?";
+        String[] a = {"East", "South", "West", "North"};
+        questionSet.add(new Question(q, a, "East"));
     }
 }
