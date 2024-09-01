a6f612899811df9dc4bf03480cf2df8a7195746a
unknown <iltfhuss@gmail.com>
2024-09-01 22:41:26 +0500

    added code

--- Multiple Choice Test/Game.java
+++ Multiple Choice Test/Game.java
@@ -1,5 +1,8 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
 
 
 public class Game {
@@ -7,8 +10,27 @@ public class Game {
 
     public Game() {
         questionSet = new ArrayList<Question>();
-        String q = "Where does the sun rize?";
+        String q = "Where does the sun rise?";
         String[] a = {"East", "South", "West", "North"};
         questionSet.add(new Question(q, a, "East"));
+        q = "Who invented the telephone?";
+        // must reuse variable with new String[]
+        // array constants can only be used during initialization
+        a = new String[]{"Thomas Edison", "Michael Faraday", "James Watt",
+            "Alexander Bell"};       
+        
+        questionSet.add(new Question(q, a "Alexander Bell"));
+        q = "Where is the capital of Japan?";
+        a = new String[]{"Beijing", "Tokyo", "Seoul", "Bankok"};
+        questionSet.add(new Question(q, a, "Tokyo"));
+        Collections.shuffle(questionSet, new Random());
+    }
+    public void start() {
+        Scanner scan = new Scanner(System.in);
+        int numCorrect = 0;
+        //show questions from questionSet
+        for(int question =0;question<questionSet.size();question++) {
+            System.out.println(questionSet.get(question).getQuestion());
+        }
     }
 }
